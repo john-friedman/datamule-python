7f99ecb53125630872f0b54e50e7d166c4785d32
John <jgfriedman99@gmail.com>
2024-09-06 15:53:43 -0700

    package is out

--- datamule/setup.py
+++ datamule/setup.py
@@ -6,7 +6,7 @@ long_description = Path("../readme.md").read_text()
 setup(
     name="datamule",
     author="John Friedman",
-    version="0.1",
+    version="0.12",
     description = "Making it easier to use SEC filings",
     long_description=long_description,
     long_description_content_type='text/markdown',
