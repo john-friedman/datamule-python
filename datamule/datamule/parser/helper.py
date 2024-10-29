0250d464716d7edba7ea3354f5a42899be6a2465
John <jgfriedman99@gmail.com>
2024-10-28 19:04:51 -0700

    fixed 10-k parser

--- datamule/datamule/parser/helper.py
+++ datamule/datamule/parser/helper.py
@@ -1,41 +1,17 @@
 from selectolax.parser import HTMLParser
 from pathlib import Path
 
-from selectolax.parser import HTMLParser
-from pathlib import Path
 
-# Version 1: Direct text content from body
-#2s
-def load_content_v1(filename):
+def load_file_content(filename):
+    #  ~ 30ms per file
     parser = HTMLParser(open(filename).read())
-    return parser.body.text(separator='\n', strip=True)
-
-# Version 2: Using traverse with text nodes
-#.36s
-def load_content_v2(filename):
-    parser = HTMLParser(open(filename).read())
-    return '\n'.join(
+    text = '\n'.join(
         node.text_content.strip() 
         for node in parser.root.traverse(include_text=True)
         if node.text_content and node.text_content.strip()
     )
-
-
-
-load_file_content = load_content_v2
-
-# def load_file_content(filename):
-#     path = Path(filename)
-#     with open(path, 'r', encoding='utf-8') as file:
-#         content = file.read()
-        
-#         if path.suffix.lower() in {'.html', '.htm'}:
-#             parser = HTMLParser(content)
-#             parser.strip_tags(['style', 'script', 'head'])  # Much faster than CSS selection
-#             content = parser.text(deep=True, separator='\n', strip=True)
-            
-#         return content.translate(str.maketrans({
-#             '\xa0': ' ', '\u2003': ' ',
-#             '\u2018': "'", '\u2019': "'",
-#             '\u201c': '"', '\u201d': '"'
-#         }))
+    return text.translate(str.maketrans({
+        '\xa0': ' ', '\u2003': ' ',
+        '\u2018': "'", '\u2019': "'",
+        '\u201c': '"', '\u201d': '"'
+    }))
