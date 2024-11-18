99429f0681e0bb7b50a90536504be40475bb14cd
John <jgfriedman99@gmail.com>
2024-11-18 11:35:19 -0800

    added license to setup.py

--- datamule/setup.py
+++ datamule/setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 from pathlib import Path
 
 long_description = Path("../readme.md").read_text(encoding='utf-8')
+license_text = Path("../LICENSE").read_text(encoding='utf-8')
 
 extras = {
     "filing_viewer": ["lxml"],
@@ -16,9 +17,10 @@ extras["all"] = list(all_dependencies)
 setup(
     name="datamule",
     author="John Friedman",
-    version="0.380",
+    version="0.381",
     description="Making it easier to use SEC filings.",
     long_description=long_description,
+    license=license_text,
     long_description_content_type='text/markdown',
     packages=find_packages(include=['datamule*']) + ['datamule.dataset_builder'],  # Just add the specific package
     url="https://github.com/john-friedman/datamule-python",
