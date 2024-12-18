eee6a99797f0c67c1498cfce40df8900b779a12f
John <jgfriedman99@gmail.com>
2024-12-17 18:58:11 -0800

    Fix readme path and workflow

--- datamule/setup.py
+++ datamule/setup.py
@@ -56,7 +56,7 @@ extras["all"] = list(all_dependencies)
 setup(
     name="datamule",
     author="John Friedman",
-    version="0.413",
+    version="0.414",
     description="Making it easier to use SEC filings.",
     long_description=long_description,
     license=license_text,
