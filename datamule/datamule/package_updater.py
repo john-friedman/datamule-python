051ae73259d9cb70e633eb990f0d5429695a9cdd
John Friedman <jgfriedman99@gmail.com>
2025-04-18 15:11:35 -0700

    fixed pkg updater

--- datamule/datamule/package_updater.py
+++ datamule/datamule/package_updater.py
@@ -9,7 +9,7 @@ class PackageUpdater():
     def __init__(self):
         pass
 
-    def update_package_data():
+    def update_package_data(self):
         # Create data directory in user's home
         data_dir = Path.home() / ".datamule"
         data_dir.mkdir(exist_ok=True)
@@ -19,13 +19,12 @@ class PackageUpdater():
         file_path = data_dir / "listed_filer_metadata.csv"
         temp_gz_path = data_dir / "listed_filer_metadata.csv.gz"
 
-        if not file_path.exists():
-            print(f"Downloading data to {data_dir}")
-            urllib.request.urlretrieve(file_url, temp_gz_path)
-            
-            with gzip.open(temp_gz_path, 'rb') as f_in:
-                with open(file_path, 'wb') as f_out:
-                    shutil.copyfileobj(f_in, f_out)
-                    
-            os.remove(temp_gz_path)
-            print(f"Data downloaded to {file_path}")
+        print(f"Downloading data to {data_dir}")
+        urllib.request.urlretrieve(file_url, temp_gz_path)
+        
+        with gzip.open(temp_gz_path, 'rb') as f_in:
+            with open(file_path, 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+                
+        os.remove(temp_gz_path)
+        print(f"Data downloaded to {file_path}")
