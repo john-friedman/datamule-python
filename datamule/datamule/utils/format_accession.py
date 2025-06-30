f1a8ffd678563e8575d09352765bd30c07c23832
John Friedman <jgfriedman99@gmail.com>
2025-06-30 15:54:58 -0700

    mod format acc

--- datamule/datamule/utils/format_accession.py
+++ datamule/datamule/utils/format_accession.py
@@ -2,9 +2,11 @@
 def format_accession(accession, format):
     if format == 'int':
         accession = int(str(accession).replace('-',''))
-    elif format == 'dashed':
+    elif format == 'dash':
         accession = str(int(str(accession).replace('-',''))).zfill(18)
         accession = f"{accession[:10]}-{accession[10:12]}-{accession[12:]}"
+    elif format == 'no-dash':
+        accession = str(int(str(accession).replace('-',''))).zfill(18)
     else:
         raise ValueError("unrecognized format")
     return accession
