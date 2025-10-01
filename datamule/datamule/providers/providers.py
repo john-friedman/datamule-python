361f299023e537564107f3a91c1fe84ace587888
John Friedman <jgfriedman99@gmail.com>
2025-09-30 19:18:14 -0700

    thinking through byoc

---
+++ datamule/datamule/providers/providers.py
@@ -0,0 +1,6 @@
+# TODO
+# make it easy for people to bring their own cloud
+
+SEC_LOOKUP_DB_ENDPOINT = ""
+SEC_FILINGS_SGML_BUCKET_ENDPOINT = "https://sec-library.datamule.xyz/" 
+SEC_FILINGS_TAR_BUCKET_ENDPOINT = "https://sec-library.datamule.xyz/tar/"
