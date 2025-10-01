b9d8314baac45e9888202c9785cc6548f76826ef
John Friedman <jgfriedman99@gmail.com>
2025-10-01 14:22:25 -0700

    implemented a dionicas changes

--- datamule/setup.py
+++ datamule/setup.py
@@ -32,7 +32,7 @@ if not os.path.exists(file_path):
 setup(
     name="datamule",
     author="John Friedman",
-    version="2.3.8",
+    version="2.3.9",
     description="Work with SEC submissions at scale.",
     packages=find_packages(include=['datamule', 'datamule.*']),
     url="https://github.com/john-friedman/datamule-python",
